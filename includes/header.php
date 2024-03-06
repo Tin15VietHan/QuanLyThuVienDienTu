411a1b35ccd81b0460de3badae7e9fcca771311b
AnhQuoc8683 <nkaquoc2002@gmail.com>
2024-03-06 16:18:59 +0700

    ok

---
+++ includes/header.php
@@ -0,0 +1,23 @@
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top d-flex align-items-center header-transparent">
+    <div class="container d-flex justify-content-between align-items-center">
+
+      <div class="logo">
+        <h1 class="text-light"><a href="index.html"><span>Moderna</span></a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+      </div>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="active " href="index.php">Home</a></li>
+          <li><a href="team.php">Team</a></li>
+          <li><a href="blog.html">Blog</a></li>
+          <li><a href="QLThuVienDienTu">Login Admin<i class='bx bx-log-in' style="font-size: large;"></i></a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+  
